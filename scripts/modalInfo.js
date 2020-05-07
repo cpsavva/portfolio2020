276c762a4e67d45ef1cdd7f9371c11f1e0c21dd5
cpsavva <csoriasavva@gmail.com>
2020-05-07 15:10:22 -0400

    mostly styling with the navbar, removed footer

--- scripts/modalInfo.js
+++ scripts/modalInfo.js
@@ -1,10 +1,16 @@
+// websites
+// Cancer Alliance : https://cancer-alliance.herokuapp.com/
+// Got Bored?: https://fam-app-94e72.firebaseapp.com
+// TrailBlazer: https://trailblazer-rcb.herokuapp.com
+// NPR Scrapping: https://npr-scrape.herokuapp.com
+
 // Variables
  var modalInfo = [
 	{
 		projectName : 'Cancer Alliance',
 		image : 'calogo_modal.png',
 		alt : 'Cancer Alliance',
-		site : 'https://cancer-alliance.herokuapp.com/',
+		site : 'https://github.com/cpsavva/gitgirls2',
 		des : 'An application created to provide a place for patients of cancer to connect with each other. Users are taken to a dashboard of the specific condition and are provided with support forum, doctor recommendations and the latest news in cancer research.',
 		tech : 'HTML5, CSS3, JavaScript, Bootstrap, Node.js, Express.js, MongoDB, React.js, Firebase',
 		coll : 'Melissa Shah, Chiara DeLucia, Kevin Lawler, Asha Tadimari',
@@ -15,7 +21,7 @@
 		projectName : 'Got Bored?',
 		image : 'gotbored_modal.png',
 		alt : 'Got Bored?',
-		site : 'https://fam-app-94e72.firebaseapp.com',
+		site : 'https://github.com/cpsavva/fam-app',
 		des : 'An application created to help parents and caregivers find activities for their children in their area. Users input their location or location of interest and the app delivers results and map locations tailored for children and their families.',
 		tech : 'HTML5, CSS3, JavaScript, Bootstrap, jQuery, Firebase, OAuth, GoogleAPI',
 		coll : 'Chiara DeLucia, Melissa Shah, Asha Tadimari',
@@ -25,7 +31,7 @@
 		projectName : 'TrailBlazer',
 		image : 'trailblazer_modal.png',
 		alt : 'TrailBlazer',
-		site : 'https://trailblazer-rcb.herokuapp.com',
+		site : 'https://github.com/cpsavva/trailblazer',
 		des : 'An application created for hikers exploring the US National Parks.',
 		tech : 'HTML5, CSS3, JavaScript, Materialize, Node.js, Express.js, MySQL, Handlebars.js',
 		coll : 'Emily Sun, Deepika Vikas, Jonathan Cruz, Smriti Shrestha',
@@ -35,7 +41,7 @@
 		projectName : 'NPR Scrapping',
 		image : 'radio2_modal.jpg',
 		alt : 'NPR Scrapping',
-		site : 'https://npr-scrape.herokuapp.com',
+		site : 'https://github.com/cpsavva/npr_scrapping',
 		des : 'A web-scrapping project that allows the user to save the latest NPR articles to a database and make comments on those saved articles.',
 		tech : 'HTML5, CSS3, JavaScript, Bootstrap, Node.js, Express.js, MongoDB, Handlebars.js',
 		coll : 'Sole Developer',
@@ -57,7 +63,7 @@ function loadModalInfo(){
 					'</h2><div class="row"><div class="col-sm-4 text-center"><img class="img-rounded" src="./assets/css/images/' 
 				    + info.image +  '" alt="' + info.alt + 
 				    '"><br /><br /><p><a id="siteLink" href="' + info.site +
-				    '" target="_blank">Website</a></p></div><div class="col-sm-8"><p>' 
+				    '" target="_blank">GitHub</a></p></div><div class="col-sm-8"><p>' 
 				    + info.des +  '</p><h3 class="modalHeading">Technologies</h3><p>' 
 				    + info.tech +  '</p><h3 class="modalHeading">Collaborators</h3><p>' 
 				    + info.coll +  '</p><h3 class="modalHeading">Role</h3><p>' 
